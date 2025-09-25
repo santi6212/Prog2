39bfa1acf7f17d86f88d6742ae61c9536b5d55c4
Santiago Salas <santiagosalas06@gmail.com>
2025-09-25 19:58:26 -0400

    practica

---
+++ practica.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	#Practicas practica pppp
+	cout<<"alguien me booleo el void, me int hasta que float, le arreglo hasta que struct";
+	
+	return 0;
+}
